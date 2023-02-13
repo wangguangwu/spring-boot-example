834852b0cac77b02c182ede42859ec76fff1dd8a
wangguangwu <w@wangguangwu.com>
2023-02-13 20:08:18 +0800

    add: springboot scan mapper

--- spring-boot-mybatis/src/main/java/com/wangguangwu/mybatis/SpringBootMybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/wangguangwu/mybatis/SpringBootMybatisApplication.java
@@ -1,8 +1,13 @@
 package com.wangguangwu.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangguangwu
+ */
+@MapperScan("com.wangguangwu.mybatis.mbg.mapper")
 @SpringBootApplication
 public class SpringBootMybatisApplication {
 
