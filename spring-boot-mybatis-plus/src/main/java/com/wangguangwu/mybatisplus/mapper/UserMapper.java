01fa1b3a10a166b7554d9739898265356807fef1
wangguangwu <wangguangwu@pukanghealth.com>
2023-03-06 18:07:59 +0800

    add: mybatis-plus-generator

---
+++ spring-boot-mybatis-plus/src/main/java/com/wangguangwu/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.mybatisplus.mapper;
+
+import com.wangguangwu.mybatisplus.entity.UserDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wangguangwu
+ * @since 2023-03-06
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+
+}
---
+++ spring-boot-mybatis-plus/src/main/java/com/wangguangwu/mybatisplus/service/UserService.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.mybatisplus.service;
+
+import com.wangguangwu.mybatisplus.entity.UserDO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author wangguangwu
+ * @since 2023-03-06
+ */
+public interface UserService extends IService<UserDO> {
+
+}
